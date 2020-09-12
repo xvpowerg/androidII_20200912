41a3cae6a513b30084c9573f87d9879a8f2fe1bc
xvpowerg <c911010083@gmail.com>
2020-09-12 10:33:33 +0800

    add code

--- Ch1_TestPerference/MainActivity.java
+++ Ch1_TestPerference/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         accountText = findViewById(R.id.account);
          passwordText = findViewById(R.id.password);
          submitBtn = findViewById(R.id.submitBtn);
+        CheckBox saveBox =  findViewById(R.id.saveCheck);
          submitBtn.setOnClickListener(v->{
              String accountStr = accountText.getText().toString();
              String passwordStr = passwordText.getText().toString();
              Intent toActivityIntent = new Intent(this,ReceiveActivity.class);
              toActivityIntent.putExtra(getString(R.string.account_key),accountStr);
              toActivityIntent.putExtra(getString(R.string.pass_key),passwordStr);
+             toActivityIntent.putExtra(getString(R.string.save_key),
+                                    saveBox.isChecked());
              startActivity(toActivityIntent);
          });
     }
--- Ch1_TestPerference/ReceiveActivity.java
+++ Ch1_TestPerference/ReceiveActivity.java
@@ -2,7 +2,9 @@ package tw.com.xvpower.ch1_testperference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,10 +14,19 @@ public class ReceiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
+        //MODE_PRIVATE 只能目前應用程式讀寫
+        //test_login Preferences 所存的xml名稱
+        //如果此xml不存在 系統會自動建立完畢後讀取
+        //如果此xml存在系統會讀取
+        SharedPreferences sp =
+                getSharedPreferences("test_login", Context.MODE_PRIVATE);
+
        Intent receiveIntent =  getIntent();
         String account = receiveIntent.getStringExtra(getString(R.string.account_key));
         String passKey = receiveIntent.getStringExtra(getString(R.string.pass_key));
+        boolean saveCheck =  receiveIntent.getBooleanExtra(getString(R.string.save_key),
+                false);
        TextView msgTxt =  findViewById(R.id.msgTxt);
-        msgTxt.setText(account+":"+passKey);
+        msgTxt.setText(account+":"+passKey+":"+saveCheck);
     }
 }
