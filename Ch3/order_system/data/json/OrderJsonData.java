ca9440dd8fc842c91f25d09d20de6022303bf64d
xvpowerg <c911010083@gmail.com>
2020-10-03 12:29:45 +0800

    Update OrderJsonData.java

--- Ch3/order_system/data/json/OrderJsonData.java
+++ Ch3/order_system/data/json/OrderJsonData.java
@@ -1,6 +1,9 @@
 package tw.com.xvpower.ch2_3_ordersystem.data.json;
 
+import com.google.gson.reflect.TypeToken;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import tw.com.xvpower.ch2_3_ordersystem.bean.Order;
@@ -17,9 +20,21 @@ public class OrderJsonData  implements OrderDao {
     }
     @Override
     public boolean createOrder(Order order) {
-        //Order轉為JSON
-        String orderJson = JsonTools.objToJson(order);
-        JsonTools.createJson(jsonFile,orderJson);
+        //先去讀取Json內的訂單
+        String orderListJson = JsonTools.readJson(jsonFile);
+        ArrayList<Order> orderList = new ArrayList<>();
+        TypeToken<ArrayList<Order>> typeToken =
+                new TypeToken<ArrayList<Order>>(){};
+        //是否有舊訂單
+        if (orderListJson != null && orderListJson.length() > 1){
+            //取得所有舊訂單資料
+            orderList = JsonTools.jsonToObj(orderListJson,typeToken);
+        }
+        //add 新訂單
+        orderList.add(order);
+        //OrderList 轉為JSON
+         orderListJson = JsonTools.objToJson(orderList);
+        JsonTools.createJson(jsonFile,orderListJson);
         //把使此JSON寫到外部資料夾
         //回傳一組id
 
