40f052b8a1d01191e8ce11323887adf5dec6c195
xvpowerg <c911010083@gmail.com>
2020-10-03 11:30:51 +0800

    Update OrderJsonData.java

--- Ch3/order_system/data/json/OrderJsonData.java
+++ Ch3/order_system/data/json/OrderJsonData.java
@@ -8,6 +8,11 @@ import tw.com.xvpower.ch2_3_ordersystem.data.OrderDao;
 public class OrderJsonData  implements OrderDao {
     @Override
     public boolean createOrder(Order order) {
+        //Order轉為JSON
+
+        //把使此JSON寫到外部資料夾
+        //回傳一組id
+
         return false;
     }
 
