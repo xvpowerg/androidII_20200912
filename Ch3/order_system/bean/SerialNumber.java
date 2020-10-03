97f36861f9a3c1c3d162a293cdd2d20936025f2c
xvpowerg <c911010083@gmail.com>
2020-10-03 15:06:28 +0800

    Update SerialNumber.java

--- Ch3/order_system/bean/SerialNumber.java
+++ Ch3/order_system/bean/SerialNumber.java
@@ -1,6 +1,13 @@
 package tw.com.xvpower.ch2_3_ordersystem.bean;
 
 import android.util.Log;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+
+import tw.com.xvpower.ch2_3_ordersystem.data.json.JsonTools;
+
 //產生order id
 public class SerialNumber {
         private int id;
@@ -19,7 +26,19 @@ public class SerialNumber {
         id++;
     }
 
-    public static int getSerial(){
-        return 0;
+    public static int getSerial(File dir){
+        File serialFile = new File(dir,"SerialNumber.json");
+       String json =  JsonTools.readJson(serialFile);
+        SerialNumber serialNumber=null;
+       if (json == null || json.isEmpty()){
+           serialNumber = new SerialNumber();
+       }else{
+           TypeToken<SerialNumber> typeToKen = new TypeToken<SerialNumber>(){};
+           serialNumber = JsonTools.jsonToObj(json,typeToKen);
+       }
+        serialNumber.accumulate();
+       json = JsonTools.objToJson(serialNumber);
+        JsonTools.createJson(serialFile,json);
+        return serialNumber.id;
     }
 }
