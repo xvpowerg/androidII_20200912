47df0e18ca2ef55439ea461b3d86f96b46f58ca0
xvpowerg <c911010083@gmail.com>
2020-10-03 11:37:24 +0800

    Update JsonTools.java

--- Ch3/order_system/data/json/JsonTools.java
+++ Ch3/order_system/data/json/JsonTools.java
@@ -8,6 +8,7 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
@@ -20,6 +21,20 @@ public class JsonTools {
             }catch(IOException ex){
                 Log.e("Howard","createJson:"+ex);
             }
+    }
+
+    public static String readJson(File jsonFile){
+            StringBuilder sb = new StringBuilder();
+            try(FileReader fileReader = new FileReader(jsonFile)){
+                char[] buffer = new char[128];
+                int index = -1;
+                while((index = fileReader.read(buffer)) != -1) {
+                    sb.append(buffer,0,index);
+                }
+            }catch (IOException ex){
+                Log.e("Howard","readJson:"+ex);
+            }
+            return sb.toString();
     }
 
     public static <T> String objToJson(T obj){
